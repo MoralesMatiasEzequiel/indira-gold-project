730665ece66e466a03a1ad6ed463ffb9345d5fa0
Matías Morales <programacionfrida@gmail.com>
2024-06-12 16:45:18 -0300

    getClientByEmailCtrls

---
+++ server/src/controllers/clientCtrls/getClientByEmailCtrl.js
@@ -0,0 +1,11 @@
+require('../../db.js');
+const Client = require('../../collections/Client.js');
+
+const getClientByEmailCtrl = async (email) => {
+  if (email) {
+    const clientByEmail = await Client.findOne({ email: email });
+    return clientByEmail;
+  }
+};
+
+module.exports = getClientByEmailCtrl;    
