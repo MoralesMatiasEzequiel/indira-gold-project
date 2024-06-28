0a896d95d31ab1bc0fdfea45bef12bd69550990f
Matías Morales <programacionfrida@gmail.com>
2024-06-28 16:24:20 -0300

    TotalPrice de sale OK

--- server/src/controllers/saleCtrls/postSaleCtrl.js
+++ server/src/controllers/saleCtrls/postSaleCtrl.js
@@ -1,14 +1,22 @@
 require('../../db.js');
 const Sale = require('../../collections/Sale.js');
+const Product = require('../../collections/Product.js');
+
 
 const postSaleCtrl = async (orderNumber, paymentMethod, soldIn, discount, products) => {
-  
+
+    // Obtengo los productos desde la base de datos usando sus IDs
+    const productsID = await Product.find({ '_id': { $in: products } });
+
+    // Calculo el precio total sumando los precios de los productos
+    const totalPrice = productsID.reduce((total, product) => total + product.price, 0);
     const newSale = {
         orderNumber,
         paymentMethod,
         soldIn,
         discount,
-        products
+        products,
+        totalPrice
     }
 
     const saleCreated = await Sale.create(newSale);
