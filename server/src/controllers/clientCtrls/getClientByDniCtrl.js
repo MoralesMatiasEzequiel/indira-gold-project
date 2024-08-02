1f030d8b6f31b5cbbffd47a34e88517c2da10f19
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2024-08-02 11:35:07 -0300

    getClientsByDni fixed

--- server/src/controllers/clientCtrls/getClientByDniCtrl.js
+++ server/src/controllers/clientCtrls/getClientByDniCtrl.js
@@ -2,12 +2,16 @@ require('../../db.js');
 const Client = require('../../collections/Client.js');
 
 const getClientByDniCtrl = async (dni) => {
+
+  const regex = new RegExp(`.*${dni}.*`, 'i');
+
   if (dni) {
-    const clientByDni = await Client.findOne({ dni: dni }).populate({
-      path: 'purchases'
-  })
-    return clientByDni;
-  }
+    const clients = await Client.find({ dni: regex })
+    .populate({
+        path: 'purchases'
+    });
+    return clients;
+  };
 };
 
 module.exports = getClientByDniCtrl;    
