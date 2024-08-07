9fc7b3fb47ca4b186d12ebd67ff9ec16bec9199e
Matías Morales <programacionfrida@gmail.com>
2024-08-07 14:00:31 -0300

    Resolve error component Stats in progress

--- server/src/collections/Sale.js
+++ server/src/collections/Sale.js
@@ -50,6 +50,10 @@ const saleSchema = new Schema({
             sizeId: {
                 type: Schema.Types.ObjectId,
                 required: true
+            },
+            category: {
+                type: String,
+                required: true
             }
         }
     ],
