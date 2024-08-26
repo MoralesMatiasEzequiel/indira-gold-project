a276bfc30f25face84a03e39e8821dce52aea63c
Matías Morales <programacionfrida@gmail.com>
2024-08-26 20:32:20 -0300

    putIncreasePrice OK

--- server/src/controllers/productCtrls/putIncreasePriceCtrl.js
+++ server/src/controllers/productCtrls/putIncreasePriceCtrl.js
@@ -1,23 +1,29 @@
 const Product = require('../../collections/Product.js');
 
-const putIncreasePriceCtrl = async (porcentage, category) => {
+const putIncreasePriceCtrl = async (porcentage, products, category) => {
     try {
-        let products;
+        let productsDB;
 
-        if (category && category.length > 0) {
+        if (products && products.length > 0) {
+            // Obtener los productos que coincidan con los IDs especificados en el array `products`
+            productsDB = await Product.find({
+                _id: { $in: products.map(p => p.value) },
+                active: true
+            });
+        } else if (category && category.length > 0) {
             // Obtener los productos que coincidan con la categoría especificada
             const categoryId = category[0].value; // Tomamos el id de la primera categoría del array
-            products = await Product.find({ 
-                category: categoryId, 
-                active: true 
+            productsDB = await Product.find({
+                category: categoryId,
+                active: true
             }).populate('category');
         } else {
-            // Si no se especifica categoría, obtener todos los productos activos
-            products = await Product.find({ active: true });
+            // Si no se especifica categoría ni productos, obtener todos los productos activos
+            productsDB = await Product.find({ active: true });
         }
 
         // Aumentar el precio en el porcentaje especificado
-        const updatedProducts = await Promise.all(products.map(async (product) => {
+        const updatedProducts = await Promise.all(productsDB.map(async (product) => {
             product.price = product.price + (product.price * (porcentage / 100));
             await product.save(); // Guardar los cambios en la base de datos
             return product;
