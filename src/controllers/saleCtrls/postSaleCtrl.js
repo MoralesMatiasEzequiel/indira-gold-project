0adf722380d31a785be754fdb8511e8548a19693
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2024-09-14 18:31:15 -0300

    subtotal calculated from price sent at front

--- src/controllers/saleCtrls/postSaleCtrl.js
+++ src/controllers/saleCtrls/postSaleCtrl.js
@@ -1,19 +1,12 @@
 require('../../db.js');
 const Sale = require('../../collections/Sale.js');
-const Product = require('../../collections/Product.js');
 
 const postSaleCtrl = async (paymentMethod, installments, soldAt, discount, products, client, paymentFee) => {
-    // Extraigo los IDs de producto únicos de `products`
-    const uniqueProductIds = [...new Set(products.map(p => p.productId))];
 
-    // Obtengo los productos desde la base de datos usando sus IDs únicos
-    const productsFromDB = await Product.find({ '_id': { $in: uniqueProductIds } });
-
-    // Calculo el precio total sumando los precios de los productos considerando las repeticiones
     const subTotal = products.reduce((total, product) => {
-        const productFromDB = productsFromDB.find(p => p._id.toString() === product.productId.toString());
-        return total + (productFromDB ? productFromDB.price : 0);
+        return total + (product.price || 0); // Asegúrate de que cada producto tenga una propiedad price
     }, 0);
+
 
     let totalPrice = subTotal;
     let discountApplied = 0;
--- src/controllers/saleCtrls/putSaleCtrl.js
+++ src/controllers/saleCtrls/putSaleCtrl.js
@@ -1,19 +1,12 @@
 require('../../db.js');
 const Sale = require('../../collections/Sale.js');
-const Product = require('../../collections/Product.js');
 
 const putSaleCtrl = async (_id, products, discount, paymentFee) => {
 
-    const uniqueProductIds = [...new Set(products.map(p => p.productId))];
-
-    // Obtengo los productos desde la base de datos usando sus IDs únicos
-    const productsFromDB = await Product.find({ '_id': { $in: uniqueProductIds } });
-
-    // Calculo el precio total sumando los precios de los productos considerando las repeticiones
     const subTotal = products.reduce((total, product) => {
-        const productFromDB = productsFromDB.find(p => p._id.toString() === product.productId.toString());
-        return total + (productFromDB ? productFromDB.price : 0);
+        return total + (product.price || 0); // Asegúrate de que cada producto tenga una propiedad price
     }, 0);
+
 
     let totalPrice = subTotal;
     let discountApplied = 0;
