10caa273bcd821cf6b59d2da98b0f1ecc4b163bb
Matías Morales <programacionfrida@gmail.com>
2024-09-21 12:48:02 -0300

    img putproduct ok

--- src/controllers/productCtrls/putProductCtrl.js
+++ src/controllers/productCtrls/putProductCtrl.js
@@ -1,6 +1,7 @@
 const Product = require('../../collections/Product.js');
 
 const putProductCtrl = async (_id, name, parsedColor, supplier, price, category, description, active, imageGlobalPath) => {
+    
     const updateFields = {
         name,
         color: parsedColor,
--- src/handlers/productHandlers/putProductHandler.js
+++ src/handlers/productHandlers/putProductHandler.js
@@ -1,16 +1,16 @@
 const putProductCtrl = require('../../controllers/productCtrls/putProductCtrl.js');
 
 const putProductHandler = async (req, res) => {
-    console.log('Incoming files:', req.files);
-    console.log('req.files:', req.files);
-    try {
-        const { _id, name, color, supplier, price, description, category, active } = req.body;
+    // console.log('Incoming files:', req.files);
+    // console.log('req.files:', req.files);
+    const { _id, name, color, supplier, price, description, category, active } = req.body;
 
+    try {
         if (!_id) return res.status(400).json({ error: 'Missing ID' });
 
         if (name && typeof name !== 'string') {
             return res.status(400).send({ error: 'Incorrect DataType name' });
-        }
+        };
 
         // Manejo de color opcional
         let parsedColor = [];
@@ -70,9 +70,9 @@ const putProductHandler = async (req, res) => {
 
         // Procesar las imágenes subidas
         const imagePaths = req.files['images'] ? req.files['images'].map(file => file.path) : [];
-        console.log('Processed image paths:', imagePaths);
+        // console.log('Processed image paths:', imagePaths);
         const imageGlobalPath = req.files['imageGlobal'] ? req.files['imageGlobal'][0].path : null;
-        console.log('Processed global image path:', imageGlobalPath);
+        // console.log('Processed global image path:', imageGlobalPath);
 
         parsedColor.forEach((c, index) => {
             if (imagePaths[index]) {
