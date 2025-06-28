2547856c825807e17e66e2a70e20a6fb5aacff3c
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2025-06-28 12:08:50 -0300

    sales by id returns debt info

--- src/controllers/saleCtrls/getSaleByIdCtrl.js
+++ src/controllers/saleCtrls/getSaleByIdCtrl.js
@@ -1,8 +1,9 @@
 require('../../db.js');
 const Sale = require('../../collections/Sale.js');
+const getActiveDebtsCtrl = require('../debtCtrls/getActiveDebtsCtrl.js');
 
 const getSaleByIdCtrl = async (_id) => {
-    
+    // Obtener la venta
     const sale = await Sale.findOne({_id})
     .populate({
         path: 'client'
@@ -10,8 +11,22 @@ const getSaleByIdCtrl = async (_id) => {
     .populate({
         path: 'products'
     });
+
+    if (!sale) return null; // Si no existe la venta, retornar null
+
+    // Obtener todas las deudas activas
+    const debts = await getActiveDebtsCtrl();
     
-    return sale;
+    // Buscar si existe una deuda para esta venta
+    const debtForSale = debts.find(debt => debt.sale._id.toString() === _id.toString());
+    
+    // Crear el objeto de venta con la información de deuda
+    const saleWithDebt = {
+        ...sale.toObject(), // Convertir el documento mongoose a objeto plano
+        debt: debtForSale ? debtForSale.remainingBalance : 0 // Asignar remainingBalance o 0 si no hay deuda
+    };
+
+    return saleWithDebt;
 }
 
 module.exports = getSaleByIdCtrl;
