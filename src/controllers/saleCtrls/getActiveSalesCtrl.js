118e04a088dfbdb269a2734d432f4c1ec054d504
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2025-06-28 11:52:49 -0300

    get active sales now return debt info

--- src/controllers/saleCtrls/getActiveSalesCtrl.js
+++ src/controllers/saleCtrls/getActiveSalesCtrl.js
@@ -1,10 +1,21 @@
 require('../../db.js');
 const Sale = require('../../collections/Sale.js');
+const getActiveDebtsCtrl = require('../debtCtrls/getActiveDebtsCtrl.js');
 
 const getActiveSalesCtrl = async () => {
+    // Obtener las deudas activas
+    const debts = await getActiveDebtsCtrl();
+
+    // Crear un mapa de deudas por saleId para acceso rápido
+    const debtMap = {};
+    debts.forEach(debt => {
+        debtMap[debt.sale._id.toString()] = debt.remainingBalance;
+    });
+
     const twoYearsAgo = new Date();
     twoYearsAgo.setFullYear(twoYearsAgo.getFullYear() - 2);
 
+    // Obtener las ventas activas
     const activeSales = await Sale.find({
         active: true,
         date: { $gte: twoYearsAgo }  
@@ -16,7 +27,16 @@ const getActiveSalesCtrl = async () => {
         path: 'products',
     });
 
-    return activeSales;
+    // Añadir la propiedad debt a cada venta
+    const salesWithDebt = activeSales.map(sale => {
+        const saleId = sale._id.toString();
+        return {
+            ...sale.toObject(), // Convertir el documento mongoose a objeto plano
+            debt: debtMap[saleId] || 0 // Asignar el remainingBalance o 0 si no existe
+        };
+    });
+
+    return salesWithDebt;
 };
 
 module.exports = getActiveSalesCtrl;
